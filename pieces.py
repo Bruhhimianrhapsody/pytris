f01f6a4dab5787a0cca4d62e92b1ab770edcc85b
Mario Maliqi <maliqimario@gmail.com>
2020-12-24 22:33:14 +0100

    flipping is hard

--- pieces.py
+++ pieces.py
@@ -3,27 +3,37 @@ import pygame
 class Piece():
 
     def __init__(self, screen_size):
-        self.block_size = 20         
-        self.x = (screen_size // 2) - (self.block_size // 2)
+        self.BLOCK_SIZE = 20         
+        self.x = (screen_size // 2) - (self.BLOCK_SIZE // 2)
         self.y = 0
+        self.state = 0
 
 class Long(Piece):
 
     def __init__(self, screen_size):
         super().__init__(screen_size)
-        self.form = [pygame.Rect(self.x, y, self.block_size, self.block_size,) for y in range(0, 40, 10)]
+        self.blocks = [pygame.Rect(self.x, y, self.BLOCK_SIZE, self.BLOCK_SIZE,) for y in range(0, 40, 10)]
         self.color = (64, 224, 208)
+        self.flip_state = True
 
     def show(self, screen):
-        for i in self.form:
+        for i in self.blocks:
             pygame.draw.rect(screen, self.color, i)
 
     def update(self):
-        for i in self.form:
-            i.y += self.block_size  
+        for i in self.blocks:
+            i.y += self.BLOCK_SIZE  
 
     def flip(self):
-        diff = -20
-        for i in self.form:
-            i.x += diff
-            diff += 10 
+        if self.flip_state == True:
+            diff = -20
+            for i in self.blocks:
+                i.x += diff
+                diff += self.BLOCK_SIZE
+            for i in self.blocks:
+                i.y = self.blocks[-1].y
+                self.flip_state = False
+        else:
+            
+            self.flip_state = True
+        
